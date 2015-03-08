b0abfec56d348b58f3878f087b59b03afced034f
Bartosz Ocytko <bocytko@gmail.com>
2015-03-08 22:34:50 +0100

    Added custom banner

--- src/main/java/de/zalando/mazerunner/Main.java
+++ src/main/java/de/zalando/mazerunner/Main.java
@@ -4,36 +4,44 @@ import de.zalando.mazerunner.domain.Maze;
 import de.zalando.mazerunner.domain.Mazes;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 @Configuration
 @EnableAutoConfiguration
-@EnableAsync
-@EnableScheduling
 @ComponentScan
 public class Main {
     @Bean
     public Mazes mazes() {
         Mazes mazes = new Mazes();
 
-        Maze maze = new Maze();
-        maze.setCode("maze-1");
-        maze.setWidth(3);
-        maze.setHeight(3);
-        maze.setFields("#@##.#..x");
+        Maze first = new Maze();
+        first.setCode("maze-1");
+        first.setWidth(3);
+        first.setHeight(3);
+        first.setFields("#@#" +
+                        "#.#" +
+                        "..x");
 
-        mazes.getMazes().add(maze);
+        mazes.getMazes().add(first);
+
+        Maze second = new Maze();
+        second.setCode("maze-2");
+        second.setWidth(8);
+        second.setHeight(5);
+        second.setFields("########" +
+                         "@.#....x" +
+                         "#.##.###" +
+                         "#....###" +
+                         "########");
+
+        mazes.getMazes().add(second);
 
         return mazes;
     }
 
     public static void main(final String[] args) throws Exception {
-        new SpringApplicationBuilder().showBanner(false).sources(Main.class).run(args);
+        new SpringApplicationBuilder().showBanner(true).sources(Main.class).run(args);
     }
 }
